d759f0054071553ae28255b70f257a6b1acfc385
Three6ty1 <wk.zhang02@gmail.com>
2024-02-01 02:14:28 +1100

    Removed hello api call in index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -45,7 +45,6 @@ export default function Home() {
           </div>
           <div className="flex flex-col items-center gap-2">
             <p className="text-2xl text-white">
-              {/* {hello.data ? hello.data.greeting : "Loading tRPC query..."} */}
             </p>
             <AuthShowcase />
           </div>
