fb912ff0cc11a83b3095f27f2dccbae394b5e145
Three6ty1 <wk.zhang02@gmail.com>
2024-02-02 00:20:22 +1100

    Closes #11, refetches query every 15 secs and scrolling

--- src/components/leaveSociety.tsx
+++ src/components/leaveSociety.tsx
@@ -1,5 +1,5 @@
 import { api } from "~/utils/api";
-import React from "react";
+import React, { useRef } from "react";
 import { useState } from "react";
 import {
   Modal,
@@ -24,25 +24,21 @@ type Society = {
 
 export default function LeaveSociety() {
   const total = 2;
-  const headerStep1 = "Choose a society from the list below to leave";
+  const headerStep1 = "Choose a society from the list below to leave.";
   const headerStep2 = "Are you sure you want to leave this society?";
 
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
   const [currentStep, setCurrentStep] = useState(0);
   const [chosenSociety, setChosenSociety] = useState<Society>();
-  const [societyList, setSocietyList] = useState<Society[] | undefined>(
-    undefined,
-  );
 
   const societyListArgs = api.admin.getAdminSocietyList.useQuery(undefined, {
-    enabled: societyList === undefined,
-    retry: false,
+    refetchInterval: 15000,
+    refetchIntervalInBackground: false,
     refetchOnWindowFocus: false,
   });
-  if (societyListArgs.isSuccess) {
-    setSocietyList(societyListArgs.data ? societyListArgs.data.societies : []);
-  }
+
+  const societyList = societyListArgs.data ? societyListArgs.data.societies : []
 
   const leaveSocietyMutation = api.admin.leaveSociety.useMutation();
 
@@ -51,20 +47,14 @@ export default function LeaveSociety() {
       leaveSocietyMutation.mutate({
         id: chosenSociety.id,
       });
-      if (leaveSocietyMutation.isSuccess) {
-        handleCancel();
-      } else {
-        console.log("Uh oh...: Left a society that the user was not a part of");
-      }
+      handleCancel();
     }
   };
 
   const handleSelect = (id: React.Key) => {
-    if (typeof id == "number") {
-      // Because we are preselecting, find should not return undefined...
-      setChosenSociety(societyList?.find((society) => society.id == id));
-      setCurrentStep((prev) => (prev < total - 1 ? prev + 1 : prev));
-    }
+    // Because we are preselecting, find should not return undefined...
+    setChosenSociety(societyList?.find((society) => society.id == id));
+    setCurrentStep((prev) => (prev < total - 1 ? prev + 1 : prev));
   };
 
   const handleCancel = () => {
@@ -74,13 +64,9 @@ export default function LeaveSociety() {
     setChosenSociety(undefined);
   };
 
-  const handleOpen = () => {
-    setSocietyList(undefined);
-  }
-
   return (
     <div className="flex flex-col gap-2">
-      <Button onPress={handleOpen} className="max-w-fit" color="primary">
+      <Button onPress={onOpen} className="max-w-fit" color="primary">
         Leave a society
       </Button>
       <Modal
@@ -89,50 +75,46 @@ export default function LeaveSociety() {
         onOpenChange={onOpenChange}
         onClose={() => handleCancel()}
       >
-        <ModalContent className="h-fit">
+        <ModalContent className="max-h-[50vh]">
           {(onClose) => (
             <>
               <ModalHeader className="flex flex-col items-center">
-                <span className="text-3xl font-bold">Join a society</span>
+                <span className="text-3xl font-bold">Leave a society</span>
               </ModalHeader>
-              <ModalBody>
+              <ModalBody className="flex flex-col items-center overflow-y-scroll">
                 <p>{currentStep == 0 ? headerStep1 : headerStep2}</p>
                 <Progress
                   aria-label="Form progress"
                   value={(currentStep / (total - 1)) * 100}
                   className="max-w-sm py-2"
                 />
-                <div>
-                  {currentStep === 0 && (
-                    <div className="w-full max-w-[260px] rounded-small border-small border-default-200 px-1 py-2 dark:border-default-100">
+                <div className='w-full overflow-y-scroll'>
+                  {currentStep === 0 && societyList &&
+                    <div className="flex max-w-sm h-fit rounded-small border-small border-default-200 px-1 py-2 dark:border-default-100">
                       <Listbox
-                        classNames={{
-                          base: "max-w-xs",
-                          list: "max-h-[300px] overflow-scroll",
-                        }}
                         items={societyList}
                         label="Society List"
                         onAction={(key: React.Key) => handleSelect(key)}
                         variant="flat"
                       >
                         {(item) => (
-                          <ListboxItem key={item.id} textValue={item.name}>
-                            <div className="flex items-center gap-2">
+                          <ListboxItem key={item.id} textValue={item.name} showDivider={true}>
+                            <div className="flex flex-row items-center gap-2">
                               <Avatar
                                 alt={item.name}
                                 className="flex-shrink-0"
-                                size="sm"
+                                size="md"
                                 src={item.image ? item.image : DefaultIcon.src}
                               />
                               <div className="flex flex-col">
-                                <span className="text-small">{item.name}</span>
+                                <span>{item.name}</span>
                               </div>
                             </div>
                           </ListboxItem>
                         )}
                       </Listbox>
                     </div>
-                  )}
+                  }
                   {currentStep === total - 1 && chosenSociety && (
                     <div className="flex flex-row items-center justify-center space-x-4">
                       <Avatar
