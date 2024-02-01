aa69f526accaf7c53de1f2b0179dfbadc84d5020
Three6ty1 <wk.zhang02@gmail.com>
2024-02-01 21:25:28 +1100

    85% Finished logic with leaveSociety

--- src/components/leaveSociety.tsx
+++ src/components/leaveSociety.tsx
@@ -10,32 +10,36 @@ import {
   Button,
   useDisclosure,
   Progress,
-  Input,
   Avatar,
+  Listbox,
+  ListboxItem,
 } from "@nextui-org/react";
 import DefaultIcon from "../../public/default.png";
 
 type Society = {
-    name: string;
-    id: number;
-    image: string | null;
-}
+  name: string;
+  id: number;
+  image: string | null;
+};
 
 export default function LeaveSociety() {
   const total = 2;
-  const color = "primary";
-  const variant = "flat";
-  const headerStep1 =
-    "Choose a society from the list below to leave";
+  const headerStep1 = "Choose a society from the list below to leave";
   const headerStep2 = "Are you sure you want to leave this society?";
 
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
   const [currentStep, setCurrentStep] = useState(0);
   const [chosenSociety, setChosenSociety] = useState<Society>();
-  const [societyList, setSocietyList] = useState<Society[] | undefined>(undefined);
+  const [societyList, setSocietyList] = useState<Society[] | undefined>(
+    undefined,
+  );
 
-  const societyListArgs = api.admin.getAdminSocietyList.useQuery(undefined, {enabled: societyList === undefined, retry: false, refetchOnWindowFocus: false});
+  const societyListArgs = api.admin.getAdminSocietyList.useQuery(undefined, {
+    enabled: societyList === undefined,
+    retry: false,
+    refetchOnWindowFocus: false,
+  });
   if (societyListArgs.isSuccess) {
     setSocietyList(societyListArgs.data ? societyListArgs.data.societies : []);
   }
@@ -44,19 +48,30 @@ export default function LeaveSociety() {
 
   const handleSubmit = () => {
     if (chosenSociety) {
-        leaveSocietyMutation.mutate({
-            id: chosenSociety.id,
-        });
-
+      leaveSocietyMutation.mutate({
+        id: chosenSociety.id,
+      });
+      if (leaveSocietyMutation.isSuccess) {
+        // Force refetch of society list due to a successful leave request
+        setSocietyList(undefined);
         handleCancel();
+      } else {
+        console.log("Uh oh...: Left a society that the user was not a part of");
+      }
     }
   };
 
-  // () => setCurrentStep((prev) => (prev < total - 1 ? prev + 1 : prev))
+  const handleSelect = (id: React.Key) => {
+    if (typeof id == "number") {
+      // Because we are preselecting, find should not return undefined...
+      setChosenSociety(societyList?.find((society) => society.id == id));
+      setCurrentStep((prev) => (prev < total - 1 ? prev + 1 : prev));
+    }
+  };
 
   const handleCancel = () => {
     console.log("Handling Cancel");
-    setSocietyList(undefined); 
+    // Do not refresh the society list if there was no change.
     setCurrentStep(0);
     setChosenSociety(undefined);
   };
@@ -87,9 +102,34 @@ export default function LeaveSociety() {
                 />
                 <div>
                   {currentStep === 0 && (
-                    <>
-                        {/** Society . map into a list*/}
-                    </>
+                    <div className="w-full max-w-[260px] rounded-small border-small border-default-200 px-1 py-2 dark:border-default-100">
+                      <Listbox
+                        classNames={{
+                          base: "max-w-xs",
+                          list: "max-h-[300px] overflow-scroll",
+                        }}
+                        items={societyList}
+                        label="Society List"
+                        onAction={(key: React.Key) => handleSelect(key)}
+                        variant="flat"
+                      >
+                        {(item) => (
+                          <ListboxItem key={item.id} textValue={item.name}>
+                            <div className="flex items-center gap-2">
+                              <Avatar
+                                alt={item.name}
+                                className="flex-shrink-0"
+                                size="sm"
+                                src={item.image ? item.image : DefaultIcon.src}
+                              />
+                              <div className="flex flex-col">
+                                <span className="text-small">{item.name}</span>
+                              </div>
+                            </div>
+                          </ListboxItem>
+                        )}
+                      </Listbox>
+                    </div>
                   )}
                   {currentStep === total - 1 && chosenSociety && (
                     <div className="flex flex-row items-center justify-center space-x-4">
