ba7272b0c7f8942ac4b26fa31bd49319c884bb97
Three6ty1 <wk.zhang02@gmail.com>
2024-02-01 20:18:27 +1100

    Changed wording to be join instead of submit

--- src/components/joinSociety.tsx
+++ src/components/joinSociety.tsx
@@ -160,7 +160,7 @@ export default function JoinSociety() {
                         onClose();
                       }}
                     >
-                      Submit
+                      Join
                     </Button>
                   </div>
                 )}
