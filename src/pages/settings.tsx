618739f41762f36c786aae1544ae1e6d776bba6f
HARI-PRMD <haripramodb@gmail.com>
2024-02-02 17:35:35 +1100

    retrieved user session from server

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,50 +1,33 @@
-import type { GetServerSidePropsContext, InferGetServerSidePropsType, GetServerSideProps } from "next"
-import JoinSociety from "~/components/joinSociety"
-import LeaveSociety from "~/components/leaveSociety"
-import { authOptions, getServerAuthSession } from "~/server/auth"
-import { Session } from "next-auth"
+import JoinSociety from "~/components/joinSociety";
+import LeaveSociety from "~/components/leaveSociety";
+import { getServerAuthSession } from "~/server/auth";
+import { Session } from "next-auth";
+import {
+  type InferGetServerSidePropsType,
+  type GetServerSideProps,
+  type NextPage,
+} from "next";
+import { useSession } from "next-auth/react";
 
-/**
-export const getServerSideProps = async (context: {req: GetServerSidePropsContext["req"], res: GetServerSidePropsContext["res"]}) => {
-  const session = await getServerAuthSession(context);
-  if (!session) {
-    return {
-      redirect: {
-        destination: '/',
-        permanent: false,
-      },
-    }
-  }
-
-  console.log({props: {
-    session,
-  }})
-
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const session = await getServerAuthSession(ctx);
   return {
-    props: {
-      session,
-    },
-  }
-*/
-export const getServerSideProps: GetServerSideProps<{
-    session: Session | null;
-  }> = async ctx => {
-    return {
-      props: {
-        session: await getServerAuthSession(ctx),
-      },
-    };
+    props: { session },
+  };
 };
 
-export default function Settings({ session }: any) {
-  console.log(session)
-  
+const Settings = () => {
+  const { data: session } = useSession();
+  console.log(session);
+
   return (
     <main className=" flex min-h-screen flex-col items-center justify-start">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-      <JoinSociety />
-      {/**<LeaveSociety isAuthorized={session.user.type === "ADMIN"}/>*/}
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+        <JoinSociety />
+        <LeaveSociety isAuthorized={session?.user.type === "ADMIN"} />
       </div>
     </main>
-  )
-}
+  );
+};
+
+export default Settings;
