a2e8f68b2546ec4515196d81e49ae2deb6b1e579
Three6ty1 <wk.zhang02@gmail.com>
2024-02-03 02:05:46 +1100

    Styling for icons and name

--- src/pages/leaderboard.tsx
+++ src/pages/leaderboard.tsx
@@ -6,11 +6,20 @@ import {
   TableBody,
   TableRow,
   TableCell,
-  Pagination,
   getKeyValue,
+  Avatar,
 } from "@nextui-org/react";
 import { api } from "~/utils/api";
 import StatusProgressBar from "~/components/statusProgressBar";
+
+import DefaultIcon from "../../public/default.png";
+
+type User = {
+  id: number;
+  name: string | null;
+  image: string | null;
+  points: number;
+};
 
 export default function App() {
   const [page, setPage] = useState(1);
@@ -23,7 +32,9 @@ export default function App() {
     refetchIntervalInBackground: false,
   });
 
-  const leaderboard = leaderboardArgs.isSuccess ? leaderboardArgs.data : [];
+  const leaderboard: User[] = leaderboardArgs.isSuccess
+    ? leaderboardArgs.data
+    : [];
 
   const pages = Math.ceil(leaderboard.length / rowsPerPage);
 
@@ -34,30 +45,62 @@ export default function App() {
     return leaderboard.slice(start, end);
   }, [page, leaderboard]);
 
+  const renderCell = React.useCallback(
+    (index: number, user: User, columnKey: React.Key) => {
+      switch (columnKey) {
+        case "index":
+          return <p>{index}</p>;
+        case "name":
+          return (
+            <div className="flex flex-row items-center space-x-4">
+              <div className="">
+                <Avatar
+                  size="sm"
+                  src={user.image ? user.image : DefaultIcon.src}
+                />
+              </div>
+              <p>{user.name}</p>
+            </div>
+          );
+        case "points":
+          return <p>{Number(user.points)}</p>;
+        default:
+          return user[columnKey as keyof typeof user];
+      }
+    },
+    [],
+  );
+
   return (
-    <main className="flex h-screen flex-col justify-start overflow-y-clip">
-      <div className="container flex w-full h-full flex-col items-center justify-top gap-4 space-y-2 p-4">
+    <main className="flex h-screen flex-col justify-start overflow-y-clip p-6">
+      <div className="justify-top container flex h-full w-full flex-col items-center gap-4 space-y-2">
         <h1>Leaderboards</h1>
         <StatusProgressBar />
         <div className="flex h-full w-full overflow-y-scroll">
-          <Table aria-label="Example table with dynamic content" className="w-full" isHeaderSticky={true}>
-            <TableHeader columns={[{key: "name", label: "Name"}, {key: "points", label: "Points"}]}>
-              {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
-            </TableHeader>
-            <TableBody items={leaderboard}>
-              {(item) => (
-                <TableRow>
-                  {(columnKey) =>
-                    columnKey === "points" ? (
-                      <TableCell>
-                        {Number(getKeyValue(item, columnKey))}
-                      </TableCell>
-                    ) : (
-                      <TableCell>{getKeyValue(item, columnKey)}</TableCell>
-                    )
-                  }
-                </TableRow>
+          <Table
+            aria-label="Example table with custom cells"
+            isHeaderSticky={true}
+            isStriped={true}
+            classNames={{wrapper: "p-4 pt-0 shadow-none"}}
+          >
+            <TableHeader
+              columns={[
+                { key: "name", label: "Name" },
+                { key: "points", label: "Points" },
+              ]}
+            >
+              {(column) => (
+                <TableColumn key={column.key}>{column.label}</TableColumn>
               )}
+            </TableHeader>
+            <TableBody>
+              {items.map((item, index) => (
+                <TableRow key={item.id}>
+                  {(columnKey) => (
+                    <TableCell>{renderCell(index, item, columnKey)}</TableCell>
+                  )}
+                </TableRow>
+              ))}
             </TableBody>
           </Table>
         </div>
