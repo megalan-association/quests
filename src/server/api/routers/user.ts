69c11ff6647f3d0ef3dd5c6be273cb2c70f72c44
Three6ty1 <wk.zhang02@gmail.com>
2024-02-02 18:30:19 +1100

    Added profanity filtering on change name, updated package-lock

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -1,5 +1,6 @@
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
+import { CensorSensor } from 'censor-sensor';
 
 import {
   createTRPCRouter,
@@ -8,8 +9,17 @@ import {
 
 export const UserRouter = createTRPCRouter({
   changeName: protectedProcedure
-    .input(z.object({ newName: z.string().min(2) }))
+    .input(z.object({ newName: z.string().min(2)}))
     .mutation(async ({ ctx, input }) => {
+      const censor = new CensorSensor();
+      // Tier 1: Slurs, Tier 3: Sexual terms
+      censor.disableTier(4);    // British profanity (lol)
+      censor.disableTier(2);    // Common Profanity
+
+      if (censor.isProfaneIsh(input.newName)) {
+        throw new TRPCError({ code: "PARSE_ERROR", message: "New name contains profanity"})
+      }
+
       return ctx.db.user.update({
         where: {
           id: ctx.session.user.id,
