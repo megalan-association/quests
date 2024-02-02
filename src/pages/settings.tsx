3bdaf308343ab4c83b50454cd3a5729f5f54b6dd
Three6ty1 <wk.zhang02@gmail.com>
2024-02-02 17:47:15 +1100

    Added hello username field

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,28 +1,18 @@
 import JoinSociety from "~/components/joinSociety";
 import LeaveSociety from "~/components/leaveSociety";
 import { getServerAuthSession } from "~/server/auth";
-import { Session } from "next-auth";
 import {
-  type InferGetServerSidePropsType,
   type GetServerSideProps,
-  type NextPage,
 } from "next";
 import { useSession } from "next-auth/react";
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const session = await getServerAuthSession(ctx);
-  return {
-    props: { session },
-  };
-};
-
 const Settings = () => {
-  const { data: session } = useSession();
-  console.log(session);
+  const { data: session } = useSession({ required: true });
 
   return (
     <main className=" flex min-h-screen flex-col items-center justify-start">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+        <h1>Hello {session?.user.name}</h1>
         <JoinSociety />
         <LeaveSociety isAuthorized={session?.user.type === "ADMIN"} />
       </div>
@@ -31,3 +21,19 @@ const Settings = () => {
 };
 
 export default Settings;
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const session = await getServerAuthSession(ctx);
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    }
+  }
+
+  return {
+    props: { session },
+  };
+};
