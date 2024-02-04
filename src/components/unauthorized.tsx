b5f4abb9786852a451bea66e1e31890d6d4f35ee
Three6ty1 <wk.zhang02@gmail.com>
2024-02-05 02:22:19 +1100

    Changed text size to be smaller than MegaLan logo

--- src/components/unauthorized.tsx
+++ src/components/unauthorized.tsx
@@ -4,12 +4,13 @@ import Layout from "~/pages/_layout";
 export default function UnAuthorized() {
   return (
     <Layout>
-      <main className="flex flex-col text-center items-center gap-4 px-4 text-3xl">
+      <main className="flex flex-col text-center items-center gap-4 px-4 text-xl">
+        <h1 className="pt-6 text-3xl font-bold">Unauthorised</h1>
         <div>You are do not have the Admin permissions to view this page.</div>
         <div>
           If you are an <span className="font-bold">Exec</span>, please join a
           society through the{" "}
-          <Link href="/settings" className="text-3xl font-bold">
+          <Link href="/settings" className="text-xl font-bold">
             Settings page
           </Link>
           .
