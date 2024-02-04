3bab706c59b7e37acb24fdf5b556f3287170aecb
max8539 <maxyuen8539@gmail.com>
2024-02-05 02:50:39 +1100

    Add links to buttons

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -5,6 +5,7 @@ import Layout from "./_layout"
 import { TrophyIcon } from "~/components/icons/TrophyIcon"
 import { UserIcon } from "~/components/icons/UserIcon"
 import { GiftIcon } from "~/components/icons/GiftIcon"
+import Link from "next/link"
 
 const Landing = () => {
   return (
@@ -13,9 +14,11 @@ const Landing = () => {
         <Card className="w-full md:w-[752px] mx-auto">
           <CardBody>
             <h1>MegaLAN logo here</h1>
-            <p>Catchphrase here</p>
+            <p>Tagline here</p>
             <div className="text-center py-1">
               <Button
+                as={Link}
+                href="/api/auth/signin"
                 color="primary"
                 startContent={<UserIcon />}
                 className="font-bold"
@@ -25,6 +28,8 @@ const Landing = () => {
             </div>
             <div className="text-center py-1">
               <Button
+                  as={Link}
+                  href="/leaderboard"
                   color="primary"
                   startContent={<TrophyIcon />}
                   className="font-bold mr-2"
@@ -32,6 +37,11 @@ const Landing = () => {
                   Leaderboard
               </Button>
               <Button
+                  /*
+                    Uncomment below attributes and fill in href when page exists
+                    as={Link}
+                    href=""
+                  */
                   color="primary"
                   startContent={<GiftIcon />}
                   className="font-bold mr-1"
